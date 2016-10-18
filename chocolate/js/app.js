34962d1719f8d7d6f9429444a0ea6f5d011506d3
editt-editt <editt.dabrowska@gmail.com>
2016-10-18 22:08:04 +0200

    choco v.03

--- chocolate/js/app.js
+++ chocolate/js/app.js
@@ -1,3 +1,39 @@
 document.addEventListener('DOMContentLoaded', function(){
-  
+  var $navigation = document.querySelector('nav');
+  var $menuUl = $navigation.querySelector('.menu');
+  var $links = Array.from($menuUl.querySelectorAll('a'));
+  var $menuBar = $menuUl.querySelector('i');
+
+    mobileMenu();
+    window.addEventListener('resize', function(){
+      mobileMenu();
+    });
+
+    console.log($navigation);
+
+    $menuBar.addEventListener('click', function(){
+      mobileMenuShowPosition();
+    });
+
+  function mobileMenu(){
+    if(window.matchMedia("(min-width: 601px)").matches){
+      //turn off mobile menu
+      if($menuUl.classList.contains('mobile')){
+        $menuUl.classList.remove('mobile');
+        $links.forEach(function($element){
+          $element.classList.remove('show');
+        });
+      }
+    } else {
+      //turn on mobile menu
+      console.log('mobile');
+      $menuUl.classList.add('mobile');
+    }
+  }
+
+  function mobileMenuShowPosition(){
+    $links.forEach(function($element){
+      $element.classList.toggle('show');
+    });
+  }
 });
